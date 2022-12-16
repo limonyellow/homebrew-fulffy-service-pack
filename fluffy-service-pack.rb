d05bf84d3a0a99c87a5bf9a7ca6d749d969677a7
limony <92252922+limony-city@users.noreply.github.com>
2022-12-16 12:36:56 +0200

    Update fluffy-service-pack.rb

--- fluffy-service-pack.rb
+++ fluffy-service-pack.rb
@@ -1,8 +1,8 @@
 class FluffyServicePack < Formula
   desc "This service pack will help you relax with some fulffy sounds."
   homepage "https://github.com/limonyellow/fluffy-service-pack"
-  url "https://github.com/limonyellow/fluffy-service-pack/archive/refs/tags/v0.0.6.tar.gz"
-  sha256 "9af22e2d0fd903fedf7c42017afb64ffa3304a5cbef2892546317d0663c1ba3b"
+  url "https://github.com/limonyellow/fluffy-service-pack/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "eaf617508db86cfa2a43b9f9eeac408e17024deb78e30dc645915d7ebd2cc491"
   license "MIT"
 
   def install
